75cd1d2c6adcfd80a3f76806457648d47f75672a
桜井俊介 <tuktuk.032@gmail.com>
2018-08-23 09:35:04 +0900

    favorite system

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,6 +16,7 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.create(blog_params)
+    @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path,notice:"ブログを作成しました！"
     else
@@ -24,6 +25,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   def edit
@@ -44,6 +46,7 @@ class BlogsController < ApplicationController
 
   def confirm
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     render :new if @blog.invalid?
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     end
     resources :sessions, only:[:new,:create,:destroy]
     resources :users
+    resources :favorites,only:[:create,:destroy]
 end
